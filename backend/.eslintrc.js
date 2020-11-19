ddad5d9dd62d3f63cae64cc485110b77a159ccbc
Arshad <arshadpakkali@gmail.com>
2020-11-19 09:15:38 +0530

    {update} notification API for product subscriptions

---
+++ backend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
